e2b7486c196ae99c7549cabb3902a86d66cbe5a8
zhang-l_zhang <zhang-l_zhang@neusoft.com>
2022-11-20 22:43:10 +0800

    功能修改2022年11月16日-采购管理： 删除新增功能

--- ruoyi-ui/src/views/purchasesale/purchase/index.vue
+++ ruoyi-ui/src/views/purchasesale/purchase/index.vue
@@ -82,7 +82,7 @@
     </el-form>
 
     <el-row :gutter="10" class="mb8">
-      <el-col :span="1.5">
+      <!-- <el-col :span="1.5">
         <el-button
           type="primary"
           plain
@@ -91,7 +91,7 @@
           @click="handleAdd"
           v-hasPermi="['purchasesale:purchasesale:add']"
         >新增</el-button>
-      </el-col>
+      </el-col> -->
       <el-col :span="1.5">
         <el-button
           type="success"
